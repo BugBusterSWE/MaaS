3bc48e5736ab29727148f0aa2fc1fe06daec472a
Davide Polonio <poloniodavide@gmail.com>
2016-04-29 16:47:08 +0200

    Initial commit

---
+++ src/app/maas.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by davide on 26/03/16.
+ */
+
+import * as test from "./main";
+import * as express from "express";
+/*import {Express} from "express";*/
+
+
+let app : express.Express = express();
+
+
+test.HelloWorld("test");
