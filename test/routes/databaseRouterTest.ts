5277b87b2f78d6fb3ae0734eaba890b5ab680d9e
Davide Polonio <poloniodavide@gmail.com>
2016-05-03 20:44:25 +0200

    Add empty test

---
+++ test/routes/databaseRouterTest.ts
@@ -0,0 +1,29 @@
+/**
+ * This is the test for DatabaseRouter class
+ * Created by Davide Polonio on 03/05/16.
+ */
+
+import * as DatabaseRouter from "../../src/routes/databaseRouter";
+import * as express from "express";
+import * as Chai from "chai";
+
+describe("DatabaseRouter", () => {
+
+    /* nothing to test for now
+    let toTest : DatabaseRouter;
+    let dummyExpress : express.Express = express();
+
+    beforeEach(function () : void {
+
+        toTest = new DatabaseRouter(dummyExpress);
+    });
+
+    describe("#nameOfWhatYouAreTesting", () => {
+        it ("should <say what it should do>", () => {
+
+            // Your code here
+        });
+
+    });
+    */
+});
