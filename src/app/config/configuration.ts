c09b772d35bbcfc3bc42a676f3e8e1d789b3d17f
Matteo Di Pirro <matteodipirro@gmail.com>
2016-05-03 20:04:29 +0200

    Add typedoc

--- src/app/config/configuration.ts
+++ src/app/config/configuration.ts
@@ -1,27 +1,61 @@
 import {MongoConnection} from "MongoConnection";
 
+/**
+ * This is the base class for the configuration hierarchy. This class
+ * contains some useful information about the configuration of the
+ * environment.
+ */
 abstract class Configuration {
-  private envName : string;
-  private mongoConnection : MongoConnection;
-  private serverSecret : string;
+    /**
+     * String which represents the name of the environment.
+     */
+    private envName : string;
 
-  constructor(envName : string,
-              connection : MongoConnection,
-              serverSecret : string) {
-      this.envName = envName;
-      this.mongoConnection = connection;
-      this.serverSecret = serverSecret;
-  }
+    /**
+     * Parameters used for the connection with the MongoDB database.
+     */
+    private mongoConnection : MongoConnection;
 
-  public getEnvName() : string {
-    return this.envName;
-  }
+    /**
+     * String which is used for the JWT token encryption.
+     */
+    private serverSecret : string;
 
-  public getMongoConnection() : MongoConnection {
-    return this.mongoConnection;
-  }
+    /**
+     * Configuration constructor.
+     * @param envName The name of the environment
+     * @param connection The parameters of the MongoDB connection
+     * @param serverSecret The string for the JWT token encryption.
+     */
+    constructor(envName : string,
+                connection : MongoConnection,
+                serverSecret : string) {
+        this.envName = envName;
+        this.mongoConnection = connection;
+        this.serverSecret = serverSecret;
+    }
 
-  public getServerSecret() : string {
-    return this.serverSecret;
-  }
+    /**
+     * Getter for the environment name
+     * @returns {string} The environment name
+     */
+    public getEnvName() : string {
+      return this.envName;
+    }
+
+    /**
+     * Getter for the MongoDB connection's parameters
+     * @returns {MongoConnection} The parameters of the MongoDB connection
+     */
+    public getMongoConnection() : MongoConnection {
+      return this.mongoConnection;
+    }
+
+    /**
+     * Getter for the JWT token encryption string
+     * @returns {string} The the JWT token encryption string
+     */
+    public getServerSecret() : string {
+      return this.serverSecret;
+    }
 }
--- src/app/config/mongoConnection.ts
+++ src/app/config/mongoConnection.ts
@@ -1,9 +1,35 @@
+/**
+ * This class stores the parameters for the MondoDB connection.
+ */
 class MongoConnection {
+    /**
+     * The username of the user used for read data from the database.
+     */
     private user : string;
+
+    /**
+     * The password of the user used for read data from the database.
+     */
     private password : string;
+
+    /**
+     * The host of the database.
+     */
     private host : string;
+
+    /**
+     * The name of the database.
+     */
     private dbName : string;
 
+    /**
+     * Complete constructor
+     * @param user Username of the user used for read data from the database
+     * @param password Password of the user used for read data from the
+     * database.
+     * @param host Host of the database.
+     * @param db Name of the database.
+     */
     constructor(user : string, password : string, host : string, db : string) {
         this.user = user;
         this.password = password;
@@ -11,18 +37,36 @@ class MongoConnection {
         this.dbName = db;
     }
 
+    /**
+     * Getter for the username of the user used for read data from the database.
+     * @returns {string} The username of the user used for read data from the
+     * database.
+     */
     public getUser() : string {
         return this.user;
     }
 
+    /**
+     * Getter for the password of the user used for read data from the database.
+     * @returns {string} The password of the user used for read data from the
+     * database.
+     */
     public getPassword() : string {
         return this.password;
     }
 
+    /**
+     * Getter for the database's host.
+     * @returns {string} The host of the database.
+     */
     public getHost() : string {
         return this.host;
     }
 
+    /**
+     * Getter for database's name.
+     * @returns {string} The name of the database.
+     */
     public getDatabaseName() : string {
         return this.dbName;
     }
