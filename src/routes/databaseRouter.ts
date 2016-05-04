59c0243b35f6dbe433a5ab46a265f5cab08cd9b7
Davide Polonio <poloniodavide@gmail.com>
2016-05-04 13:35:51 +0200

    Write documentation

--- src/routes/databaseRouter.ts
+++ src/routes/databaseRouter.ts
@@ -1,19 +1,41 @@
-/**
- * This class contains API definitions for companies database connections
- *
- * Created by Davide Polonio on 03/05/16.
- */
-
 import * as express from "express";
+import * as promise from "promise";
 
+/**
+ * This class contains API definitions for companies database connections.
+ *  
+ * @history
+ * | Author | Action Performed | Data |
+ * | ---    | ---              | ---  | 
+ * | Davide Polonio | Create class | 03/05/2016 |
+ *
+ * @author Davide Polonio
+ * @license MIT
+ * 
+ */
 class DatabaseRouter {
 
+    /**
+     * @description Ref for express.
+     */
     private expressRef : express.Express;
 
-
+    /**
+     * @description Public constructor. It need a express ref.
+     * 
+     * @param expressRef {express.Express}
+     * Reference to the express application
+     * 
+     * @return {DatabaseRouter}
+     */
     constructor(expressRef : express.Express) {
 
         this.expressRef = expressRef;
     }
+    
+    
+    public request( req : express.Request, res : express.Response, next : express.NextFunction )  : Promise<any> {        
+        
+        return req.params;
+    }
 }
-// I have to export something here?
