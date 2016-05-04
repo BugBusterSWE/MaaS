52fec299699da52a1b82147273cca87ddfedc85c
Matteo Di Pirro <matteodipirro@gmail.com>
2016-05-04 14:42:45 +0200

    Add tests

---
+++ test/models/modelTest.ts
@@ -0,0 +1,22 @@
+/**
+ * This is a test class for Model class.
+ */
+
+import * as Model from "../../src/models/model";
+import * as Chai from "chai";
+
+describe("Model", () => {
+
+    /* nothing to test for now
+     let toTest : DatabaseRouter;
+     let dummyExpress : express.Express = express();
+     beforeEach(function () : void {
+     toTest = new DatabaseRouter(dummyExpress);
+     });
+     describe("#nameOfWhatYouAreTesting", () => {
+     it ("should <say what it should do>", () => {
+     // Your code here
+     });
+     });
+     */
+});
---
+++ test/models/mongooseConnectionTest.ts
@@ -0,0 +1,22 @@
+/**
+ * This is a test class for MongooseConnection class.
+ */
+
+import * as MongooseConnection from "../../src/models/mongooseConnection";
+import * as Chai from "chai";
+
+describe("MongooseConnection", () => {
+
+    /* nothing to test for now
+     let toTest : DatabaseRouter;
+     let dummyExpress : express.Express = express();
+     beforeEach(function () : void {
+     toTest = new DatabaseRouter(dummyExpress);
+     });
+     describe("#nameOfWhatYouAreTesting", () => {
+     it ("should <say what it should do>", () => {
+     // Your code here
+     });
+     });
+     */
+});
