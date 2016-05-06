606da71e626c1d305fff3f11512b06eec9295913
Matteo Di Pirro <matteodipirro@gmail.com>
2016-05-06 11:08:12 +0200

    Add router facade

---
+++ src/routes/routerFacade.ts
@@ -0,0 +1,15 @@
+/**
+ * Here we export all route files. You need only to import this file.
+ * @history
+ * |      Author     | Action Performed |      Data      |
+ * |       ---       |        ---       |       ---      |
+ * | Matteo Di Pirro | Create module     | 06/05/2016     |
+ *
+ * @author Matteo Di Pirro
+ * @license MIT
+ */
+
+export {DatabaseRouter} from "./databaseRouter";
+export {DSLRouter} from "./dslRouter";
+export {UserRouter} from "./userRouter";
+export {CompanyRouter} from "./companyRouter";
