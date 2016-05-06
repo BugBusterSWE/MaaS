80d044aea5c925b93c598208bd70073076ba7096
Davide Polonio <poloniodavide@gmail.com>
2016-05-06 11:45:20 +0200

    Add documentation on test

--- test/routes/databaseRouterTest.ts
+++ test/routes/databaseRouterTest.ts
@@ -1,13 +1,20 @@
-/**
- * This is the test for DatabaseRouter class
- * 
- * Created by Davide Polonio on 03/05/16.
- */
-
 import * as DatabaseRouter from "../../src/routes/databaseRouter";
 import * as express from "express";
 import * as Chai from "chai";
 
+/**
+ * This is the test for DatabaseRouter class
+ *
+ * @history
+ * | Author | Action Performed | Data |
+ * | ---    | ---              | ---  |
+ * | Davide Polonio | Create class | 03/05/2016 |
+ *
+ * @author Davide Polonio
+ * @copyright MIT
+ *
+ * Created by Davide Polonio on 03/05/16.
+ */
 describe("DatabaseRouter", () => {
 
     /* nothing to test for now
