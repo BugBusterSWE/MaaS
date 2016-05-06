9a013e1b1ef8fccf24a84ef8f46b3b117a87b58d
Davide Polonio <poloniodavide@gmail.com>
2016-05-06 11:00:16 +0200

    Add skeleton for databaseModel

---
+++ src/models/databaseModel.ts
@@ -0,0 +1,21 @@
+/**
+ * DatabaseModel manage all connections to MongoDB companies databases.
+ * Implements model and schema of MongooseJS.
+ *
+ * This model represent a connection to a company.
+ *
+ * @history
+ * | Author | Action Performed | Data |
+ * | ---    | ---              | ---  |
+ * | Davide Polonio | Create class | 06/05/2016 |
+ *
+ * @author Davide Polonio
+ * @copyright MIT
+ *
+ *
+ */
+
+class DatabaseModel {
+
+    
+}
