11a8fa3fcddc5b8420cebe010869031c9841a7e1
Matteo Di Pirro <matteodipirro@gmail.com>
2016-05-06 19:55:05 +0200

    Add conenction tests

--- test/models/mongooseConnectionTest.ts
+++ test/models/mongooseConnectionTest.ts
@@ -2,21 +2,58 @@
  * This is a test class for MongooseConnection class.
  */
 
-import * as MongooseConnection from "../../src/models/mongooseConnection";
+import MongooseConnection from "../../src/models/mongooseConnection";
 import * as Chai from "chai";
+import * as mongoose from "mongoose";
+import MongoConnection from "../../src/config/mongoConnection";
 
 describe("MongooseConnection", () => {
 
-    /* nothing to test for now
-     let toTest : DatabaseRouter;
-     let dummyExpress : express.Express = express();
-     beforeEach(function () : void {
-     toTest = new DatabaseRouter(dummyExpress);
-     });
-     describe("#nameOfWhatYouAreTesting", () => {
-     it ("should <say what it should do>", () => {
-     // Your code here
-     });
-     });
-     */
+    let toTest : MongooseConnection = MongooseConnection.getInstance();
+
+    describe("#SingletonTest", () => {
+        it ("Should follow the Singleton pattern", () => {
+            let anotherInstance : MongooseConnection = MongooseConnection.
+                getInstance();
+            Chai.assert(toTest == anotherInstance, "They are the same" +
+                " instance!");
+        });
+    });
+
+    describe("#Connection Opened", () => {
+        it ("Should open a connection", () => {
+            /* 1 is the "connected" state
+             * see
+             *  stackoverflow.com/questions/19599543/check-mongoose-connection-
+             *  state-without-creating-new-connection
+             */
+            setTimeout(() => Chai.expect(toTest.getRawConnection().readyState).
+            to.equal(1), 5000);
+
+        });
+    });
+
+    describe("#Connection Closed", () => {
+        it ("Should close a connection", () => {
+            /* 0 is the "disconnected" state
+             * see
+             *  stackoverflow.com/questions/19599543/check-mongoose-connection-
+             *  state-without-creating-new-connection
+             */
+            toTest.disconnect();
+            setTimeout(() => Chai.expect(toTest.getRawConnection().readyState).
+            to.equal(0), 5000);
+        });
+    });
+
+    describe("#Singleton Violation", () => {
+        it ("Should raise an error when two new are called", () => {
+            let fn : Function = function() {
+                let anotherConnection : MongooseConnection;
+                anotherConnection = new MongooseConnection(
+                    new MongoConnection("", "", "", 0, ""));
+            }
+            Chai.expect(fn).to.throw(Error);
+        });
+    });
 });
