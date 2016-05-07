ee22f60d3b4c5c8241dc87ed671f30d11d5b7815
Luca Bianco <luca.biancow@gmail.com>
2016-05-07 10:36:04 +0200

    Started the user model

---
+++ src/models/userModel.ts
@@ -0,0 +1,123 @@
+import * as mongoose from "mongoose";
+import * as crypto from "crypto";
+
+/**
+ * This is the model to represent users in MaaS. Extends model class.
+ *
+ * @history
+ * |      Author     | Action Performed |      Data      |
+ * |       ---       |        ---       |       ---      |
+ * | Luca Bianco     | Create class     | 07/05/2016     |
+ *
+ * @author Luca Bianco
+ * @license MIT
+ */
+
+class UserModel extends Model {
+
+    private PWD_DEFAULT_ITERATIONS : number = 1000;
+    private PWD_LENGHT : number = 50;
+
+    private USER_TYPES : Array = [
+        "BASE",
+        "ADMIN",
+        "OWNER",
+        "SUPERADMIN"
+    ];
+
+    constructor() {
+        super();
+    }
+
+    protected getSchema() : mongoose.Schema {
+        schema : mongoose.Schema = new mongoose.Schema({
+            username: {
+                type: String,
+                required: true,
+                sparse: true
+            },
+            passwordHashed: {
+                type: String,
+                required: true
+            },
+            passwordSalt: {
+                type: String,
+                required: true
+            },
+            passwordIterations: {
+                type: Number,
+                default: this.PWD_DEFAULT_ITERATIONS
+            },
+            level: {
+                type: String,
+                required: true,
+                enum: this.USER_TYPES,
+                default: this.USER_TYPES[0]
+            }
+        });
+
+        schema.virtual("password")
+            .set(function (password) : void {
+                this._password = password;
+                this.passwordSalt = this.generateSalt();
+                this.passwordHashed = this.hashPassword(this._password);
+            })
+            .get(function () : string {
+                return this._password;
+            });
+
+        this.setSchemaMethods(schema);
+
+        return schema;
+    }
+
+    private setSchemaMethods(schema : mongoose.Schema) : void {
+        schema.methods = {
+
+            authenticate: function (passwordText : string) : boolean {
+                return this.hashPassword(passwordText) === this.hashed_pwd;
+            },
+
+            generateSalt: function () : string {
+                return crypto.randomBytes(16).toString("base64");
+            },
+
+            hashPassword: function (password) : string {
+                return crypto
+                    .pbkdf2Sync(password,
+                        this.passwordSalt,
+                        this.passwordIterations,
+                        this.PWD_LENGTH)
+                    .toString("base64");
+            }
+        };
+    }
+
+    protected getModel() : mongoose.model<User> {
+        return mongoose.model<User> ("User", this.getSchema());
+    }
+
+
+    public login(username: string, password: string) : Promise {
+        return new Promise( function (resolve, reject) {
+            model.findOne(
+                {username: username},
+                function (error, user : mongoose.model<User>) {
+                    if (error) {
+                        reject(err);
+                    } else {
+                        if ( !user.authenticate(password)) {
+                            reject( new Error("Password non valida"));
+                        } else {
+                            delete user.passwordHashed;
+                            delete user.passwordSalt;
+                            delete user.passwordIterations;
+                            resolve(user);
+                        }
+                    }
+                })
+        });
+    }
+}
+
+export default UserModel;
