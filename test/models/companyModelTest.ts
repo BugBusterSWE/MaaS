219f598018b566b3ee9096c2a2126c5dc38d9c85
drigoni <davider1994@gmail.com>
2016-05-07 17:43:23 +0200

    Template for companyModelTest See issue #6

---
+++ test/models/companyModelTest.ts
@@ -0,0 +1,35 @@
+import * as CompanyModel from "../../src/models/companyModel";
+import * as Chai from "chai"
+
+/**
+ * CompanyModel implements the company business logic. It contains model and
+ * scheme defined by MongooseJS.
+ *
+ *
+ * @history
+ * | Author | Action Performed | Data |
+ * | ---    | ---              | ---  |
+ * | Davide Rigoni | Create class | 06/05/2016 |
+ *
+ * @author Davide Rigoni
+ * @copyright MIT
+ *
+ *
+ */
+
+describe("DatabaseModel", () => {
+
+    /*
+     let toTest:someThing; // Name of the object to test
+     // Call one of the many function that are in the Mocha framework
+     beforeEach(function ():void {
+     toTest = new someThing(); // You can do something before every test
+     });
+     // This function starts a single test
+     describe("#nameOfWhatYouAreTesting", () => {
+     it("should <say what it should do>", () => {
+     // Your code here
+     Chai.expect(toTest.sayHello(name)).to.equal("Hello Derp");
+     });
+     */
+});
