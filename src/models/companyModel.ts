ad4ce2eababbf17cb25f79689463806dc1d29021
drigoni <davider1994@gmail.com>
2016-05-07 17:34:01 +0200

    Implement some methods See issue #6

--- src/models/companyModel.ts
+++ src/models/companyModel.ts
@@ -1,6 +1,6 @@
 import * as promise from "es6-promise";
 import * as mongoose from "mongoose";
-import {Model} from "mongoose";
+import Model from "Model";
 
 /**
  * This class define the company business logic. It contains model and scheme
@@ -16,45 +16,99 @@ import {Model} from "mongoose";
  *
  */
 class CompanyModel extends Model{
-    name : string;
-    idOwner : string;
+    /**
+     * @description company's name
+     */
+    private name : string;
 
     /**
-     * @description constructor
+     * @description id of the company's owner
+     */
+    private idOwner : string;
+
+    /**
+     * @description Schema's company
+     */
+    private companySchema;
+
+    /**
+     * @description model's company
+     */
+    private company;
+
+    /**
+     * @description Constructor. It connect with the DB to find the company
      * @param name
      * @param idOwner
      * @return {CompanyModel}
      */
     constructor (name : string, idOwner : string) {
+
         this.name = name;
         this.idOwner = idOwner;
 
-        //verifico se esiste la company
+        this.companySchema = this.getConnection().Schema({
+            name: String,
+            idOwner: String
+        });
+
+        this.company = this.getConnection().model("Company",
+            this.companySchema);
     }
 
+    /**
+     * @description Return the company's name and idOwner
+     * @param name
+     */
     public getCompany(name : string) : CompanyModel{
-        //return json
-    }
+        return this.company.findOne({name: this.name},
+                function (err, user) {
+                    // TODO: Something to return
+        });
+    };
 
-    public update(c : CompanyModel) : void {
-        //return json
-    }
+    /**
+     * @description Update the data of the company
+     * @param company model
+     */
+    public update(company : CompanyModel) : void {
+        return this.company.findOneAndUpdate(
+            {name: this.name, idOwner: this.idOwner},
+            c, function (err, user) {
+            // TODO: Something to update
+        });
+    };
 
+    /**
+     * @description Delete the company from the DB
+     */
     public delete() : void {
-        //return json
+        return this.company.findOneAndRemove(
+            {name: this.name, idOwner: this.idOwner}, function (err, user) {
+                // TODO: Something to delete
+        });
+    };
+
+    /**
+     * @description Create a new company into DB and return it
+     * @param name is the company's name
+     * @param email is the owner's email
+     * @param password is the owner's password
+     * @return {CompanyModel}
+     */
+    static public createCompany(name : CompanyModel, email : string,
+                                password : string) : CompanyModel{
+    // TODO: make a company and save it
     }
 
-
-
+    /**
+     *  @description find a company in the DB
+     *  @param name of the company
+     *  @return {CompanyModel}
+     */
     static private findCompany(name : string) : CompanyModel{
-        //return json
-    }
-
-    static public createCompany(c : CompanyModel, e : string,
-                                p : string) : CompanyModel {
-        //create user
-        //return json
-    }
+        // TODO: find a company and return it or give error
+    };
 
 
 }
