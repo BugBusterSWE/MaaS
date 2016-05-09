bea39cca2e45dc98620c1384edc132a4ff2be4d3
korut94 <andreamnt94@gmail.com>
2016-05-09 13:28:08 +0200

    Insert test

--- test/lib/dslCheckerTest.ts
+++ test/lib/dslCheckerTest.ts
@@ -11,16 +11,68 @@ describe("DSLCheckTest", () => {
         toTest = new DSLChecker();
     });
 
-    // This function starts a single test
-    describe("#workWithCell", () => {
-        it ("should return true", () => {
+    /*
+     Test to show if DSLChecker check correctly a DSLStruct with inside 
+     only the root elements (Cell,Collection,Dashboard,Document)
+     */
+    describe("#workWithOnlyRoot", () => {
+        it("Cell", () => {
             Chai.expect(toTest.check({
                 root: "1",
                 "1": {
                     type: "cell",
                     pModel: {}
                 }
-            })).to.equal( true );
+            })).to.equal(true);
         });
+
+	it("Collection", () => {
+	    Chai.expect(toTest.check({
+	        root: "1",
+		"1": {
+		     type: "collection",
+		     pModel: {
+		       action: ["2"],
+		       index: "3",
+		       document: "4"		      	     
+		    }
+		},
+
+		"2": {
+		     type: "action",
+		     pModel: {}
+		},
+
+		"3": {
+		     type: "index",
+		     pModel: {
+		       column: ["5"]
+		     }
+		},
+
+		"4": {
+		     type: "document",
+		     pModel: {
+		       action: ["6"],
+		       row: ["7"]
+		     }
+		},
+		
+		"5": {
+		     type: "column",
+		     pModel: {}
+		},
+
+		"6": {
+		     type: "action",
+		     pModel: {}
+		},
+		
+		"7": {
+		     type: "row",
+		     pModel: {}
+		}
+	    })).to.equal(true);
+	});
     });
 });
