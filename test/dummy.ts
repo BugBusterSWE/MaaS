8ac6a0a6e748ec10e7622cef27393301cc44a156
Davide Polonio <poloniodavide@gmail.com>
2016-05-11 14:48:16 +0200

    Add a dummy test

---
+++ test/dummy.ts
@@ -0,0 +1,13 @@
+/**
+ * This is a dummy test. I need this only for testing purpose
+ */
+
+describe("DummyTest", () => {
+
+    describe("#nothing", () => {
+        it("should do nothing", () => {
+
+            // Your code here
+        });
+    });
+});
