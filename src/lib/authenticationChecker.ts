b72ced0242a05b8c11e5743649b8b8d762e0fa25
Matteo Di Pirro <matteodipirro@gmail.com>
2016-05-13 10:48:34 +0200

    Rename lib's files and add dcumentation

--- src/lib/authenticationChecker.ts
+++ src/lib/authenticationChecker.ts
@@ -3,8 +3,10 @@ import UserModel from "../models/userModel";
 import * as express from "express";
 import ConfigurationChooser from "../config/configurationChooser";
 import * as mongoose from "mongoose";
+
 /**
- * This class is used to check the authentication for the requests
+ * This class is used to check if the current user is correctly authenticate
+ * to the MaaS application.
  *
  * @history
  * |      Author     | Action Performed |      Data      |
@@ -15,28 +17,47 @@ import * as mongoose from "mongoose";
  * @license MIT
  */
 
-export default class AuthenticationChecker {
+class AuthenticationChecker {
 
+    /**
+     * @description Server's secret string, used for encode the JWT tokens.
+     */
     private secret : string;
 
-    private DEFAULT_EXPIRE_TIME : number = 60 * 24 * 7;
+    /**
+     * @description Request's expire time. By default it is 60*24*7.
+     */
+    private const DEFAULT_EXPIRE_TIME : number = 60 * 24 * 7;
 
-    private USERNAME_BODY_FIELD : string = "username";
-    private PASSWORD_BODY_FIELD : string = "username";
+    /**
+     * @description Default name of the 'username' field in every request.
+     */
+    private const USERNAME_BODY_FIELD : string = "username";
 
-    constructor() {
+    /**
+     * @description Default name of the 'password' field in every request.
+     */
+    private const PASSWORD_BODY_FIELD : string = "password";
 
-    }
-
+    /**
+     * Login the user.
+     * @param request The express request.
+     * <a href="http://expressjs.com/en/api.html#req">See</a> the official
+     * documentation for more details.
+     * @param response The express response object.
+     * <a href="http://expressjs.com/en/api.html#res">See</a> the official
+     * documentation for more details.
+     */
     public login(request : express.Request,
                  response : express.Response) : void {
         let username : string = request.body[this.USERNAME_BODY_FIELD];
         let password : string = request.body[this.PASSWORD_BODY_FIELD];
-        
+
         // TODO: sistemare inclusione del modello utente
         UserModel
-            .login(username, password)
-            .then(function (error : Object, user : mongoose.Model<Object>) : void {
+            .login(username, password) // Call the login method...
+            .then(function (error : Object, user : mongoose.Model<Object>) :
+                void { // ...when done, let's say it to the client
                 if (error || !user) {
                     this.loginFailed(response);
                 } else {
@@ -52,6 +73,16 @@ export default class AuthenticationChecker {
             })
     }
 
+    /**
+     * Authenticate the user.
+     * @param request The express request.
+     * <a href="http://expressjs.com/en/api.html#req">See</a> the official
+     * documentation for more details.
+     * @param response The express response object.
+     * <a href="http://expressjs.com/en/api.html#res">See</a> the official
+     * documentation for more details.
+     * @param next Function which invokes the next route handler in framework.
+     */
     public authenticate(
         request : express.Request,
         response : express.Response,
@@ -60,13 +91,13 @@ export default class AuthenticationChecker {
             request.query.token ||
             request.headers["x-access-token"];
 
-        if (!token) {
+        if (!token) { // Token not found
             this.responseTokenNotFound(response);
         } else {
             jwt.verify(token, this.secret, function (err, decoded) {
-                if (err) {
+                if (err) { // Authentication failed
                     this.responseAuthenticationFailed(response);
-                } else {
+                } else { // Success!
                     request.user = decoded;
                     next();
                 }
@@ -74,6 +105,11 @@ export default class AuthenticationChecker {
         }
     }
 
+    /**
+     * Create the JWT token for the current user.
+     * @param data User's data.
+     * @returns {string} A string which represents the JWT token created.
+     */
     private createToken(data : Object) : string {
         return jwt.sign(
             {
@@ -84,6 +120,11 @@ export default class AuthenticationChecker {
         );
     }
 
+    /**
+     * Create a parametrized response for the token not found situation.
+     * @param response The generated response with an error message which
+     * represents the "token not found" situation.
+     */
     private responseTokenNotFound(response : express.Response) : void {
         response.status(403);
         response.json({
@@ -92,6 +133,11 @@ export default class AuthenticationChecker {
         });
     }
 
+    /**
+     * Create a parametrized response for the authentication failed situation.
+     * @param response The generated response with an error message which
+     * represents the "authentication failed" situation.
+     */
     private responseAuthenticationFailed(response : express.Response) : void {
         response.status(403);
         response.json({
@@ -100,6 +146,11 @@ export default class AuthenticationChecker {
         });
     }
 
+    /**
+     * Create a parametrized response for the login failed situation.
+     * @param response The generated response with an error message which
+     * represents the "login failed" situation.
+     */
     private loginFailed(response : express.Response) : void {
         response.status(401);
         response.json({
@@ -108,3 +159,5 @@ export default class AuthenticationChecker {
         });
     }
 }
+
+export default AuthenticationChecker;
