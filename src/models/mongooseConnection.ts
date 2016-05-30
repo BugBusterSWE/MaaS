bf2959275b70e427c935daef3ae1174f9c03e29f
Davide Polonio <poloniodavide@gmail.com>
2016-05-30 15:58:29 +0200

    Fix lint errors

--- src/models/mongooseConnection.ts
+++ src/models/mongooseConnection.ts
@@ -42,9 +42,9 @@ class MongooseConnection {
      * @returns {MongooseConnection} The instance
      */
     public static getInstance() : MongooseConnection {
-        
+
         if (!MongooseConnection.instance) {
-         
+
             MongooseConnection.instance = new MongooseConnection(
                 ConfigurationChooser.getConfig().getMongoConnection()
             );
