c867878f637a80db876ad206e1fdefeaf512cbb1
EmanueleC <emanuele.carraro.94@gmail.com>
2016-05-30 12:42:26 +0200

    Add comments to companyStore file

--- src/public/src/scripts/stores/companyStore.ts
+++ src/public/src/scripts/stores/companyStore.ts
@@ -5,18 +5,57 @@ import {DispatcherCompaniesData,
 import {EventEmitter} from "events";
 import {Action} from "../dispatcher/dispatcher";
 
-let CHANGE_EVENT : string = "change";
 
+/**
+ * CompanyStore contains all the logic of all the Companies Entities.
+ *
+ * This model represent a connection to a company.
+ *
+ * @history
+ * | Author           | Action Performed          | Data       |
+ * | ---              | ---                       | ---        |
+ * | Emanuele Carraro | Create class CompanyStore | 27/05/2016 |
+ *
+ *
+ * @author Emanuele Carraro
+ * @copyright MIT
+ */
 class CompanyStore extends EventEmitter {
+    
+    /**
+     * @description string for events management.
+     */
+    private static CHANGE_EVENT : string = "change";
+    
+    /**
+     * @description Contains the data of all companies.
+     */
+    private companiesData : ICompany[] = [];
+    
+    /**
+     * @description <p>Represent the members of a company.
+     * This array changes over time (for each query).
+     * 
+     */
+    private companyMembers : IMember[] = [];
 
-    companiesData : ICompany[] = [];
-    companyMembers : IMember[] = [];
-
+    /**
+     * @description
+     * <p>This constructor calls his super constructor.
+     * It creates a companyStore and registers it to multiple
+     * dispatchers. </p>
+     * @return {CompanyStore}
+     */
     constructor() {
         super();
         this.actionRegister(this);
     }
 
+    /**
+     * @description Registers the companyStore to multiple dispatchers.
+     * @param companyStore {CompanyStore}
+     * @returns {void}
+     */
     actionRegister(companyStore : CompanyStore) : void {
 
         DispatcherCompaniesData.register(
@@ -38,27 +77,56 @@ class CompanyStore extends EventEmitter {
             }
         )
     }
-
+    
+    /**
+     * @description Update the companies array.
+     * @param data {ICompany[]} The data of the companies to update.
+     * @returns {void}
+     */
     updateData(data : ICompany[]) : void {
         this.companiesData = data;
     }
 
+    /**
+     * @description Update the members array.
+     * @param data {IMember[]} The members to update.
+     * @returns {void}
+     */
     updateMembers(data : IMember[]) : void {
         this.companyMembers = data;
     }
 
+    /**
+     * @description Add a new company.
+     * @param data {ICompany} The data of the company to add.
+     * @returns {void}
+     */
     addCompany(data : ICompany) : void {
         this.companiesData.push(data);
     }
 
+    /**
+     * @description Add a new member.
+     * @param data {IMember} The member to add.
+     * @returns {void}
+     */
     addMember(data : IMember) : void {
         this.companyMembers.push(data);
     }
 
-    getCompaniesData() : Array<ICompany> {
+    /**
+     * @description Get the data of the companies.
+     * @returns {ICompany[]}
+     */
+    getCompaniesData() : ICompany[] {
         return this.companiesData;
     }
 
+    /**
+     * @description Return the company which id equals _id.
+     * @param _id {ICompany} 
+     * @returns {ICompany}
+     */
     getCompany(_id : string) : ICompany {
         for (let i : number = 0; i < this.companiesData.length; ++i) {
             if (this.companiesData[i].id === _id) {
@@ -68,7 +136,12 @@ class CompanyStore extends EventEmitter {
         return {name: "Not defined", id: "Null"};
     }
 
-    getCompanyMembers(company_id : string) : Array<IMember> {
+    /**
+     * @description Get the members of a specific company.
+     * @param company_id {string} id of the specific company.
+     * @returns {IMember[]}
+     */
+    getCompanyMembers(company_id : string) : IMember[] {
         // Questo ciclo for e' utile? Non se ricevo i dati giusti
         // Al caricamento di showCompaniesMembers
         /*
@@ -83,24 +156,41 @@ class CompanyStore extends EventEmitter {
         return this.companyMembers;
     }
 
+    /**
+     * @description Emit changes to React components.
+     * @returns {void}
+     */
     emitChange() : void {
-        this.emit(CHANGE_EVENT);
+        this.emit(CompanyStore.CHANGE_EVENT);
     }
 
     /*
      i seguenti metodi sono richiamati nella dashboard
      */
 
+    /**
+     * @description attach a React component as a listener to this store
+     * @param callback {() => void} when a change event is triggered, the listener execute the callback
+     * @returns {void}
+     */
     addChangeListener(callback : () => void) : void {
-        this.on(CHANGE_EVENT, callback);
+        this.on(CompanyStore.CHANGE_EVENT, callback);
     }
 
+    /**
+     * @description Remove a listener.
+     * @param callback {() => void} when a change event is triggered, the listener execute the callback.
+     * @returns {void}
+     */
     removeChangeListener(callback : () => void) : void {
-        this.removeListener(CHANGE_EVENT, callback);
+        this.removeListener(CompanyStore.CHANGE_EVENT, callback);
     }
 
 }
 
+/**
+     * @description The companyStore object to export as a singleton.
+     */
 let store : CompanyStore = new CompanyStore();
 
 export default store;
