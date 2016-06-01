570e88311f5cba52366824ad1fd7dcd23b231dee
drigoni <davider1994@gmail.com>
2016-06-01 10:10:56 +0200

    Add todo

--- src/public/src/scripts/react/components/login.tsx
+++ src/public/src/scripts/react/components/login.tsx
@@ -59,6 +59,7 @@ class Login extends React.Component<ILoginProps, ILoginState> {
         }
     }
 
+    //TODO: passare parametro al messaggio di errore
     /* tslint:disable: max-line-length */
     render() : JSX.Element {
         return(
