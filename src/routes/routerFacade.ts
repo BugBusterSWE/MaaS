00a23b128fa4404f5b626860b18485b026105b6a
EmanueleC <emanuele.carraro.94@gmail.com>
2016-06-01 10:42:33 +0200

    fix error

---
+++ src/routes/routerFacade.ts
@@ -0,0 +1,46 @@
+import * as express from "express";
+
+/**
+ * Here we export all route files. You need only to import this file.
+ * @history
+ * |      Author     | Action Performed |      Data      |
+ * |       ---       |        ---       |       ---      |
+ * | Matteo Di Pirro | Create module     | 06/05/2016     |
+ *
+ * @author Matteo Di Pirro
+ * @license MIT
+ */
+
+// Import the routes
+import DSLRouter from "./dslRouter";
+import {userRouter} from "./userRouter";
+import DatabaseRouter from "./databaseRouter";
+import CompanyRouter from "./companyRouter";
+
+let RouterFacade : express.Router = express.Router();
+
+RouterFacade.use(new DSLRouter().getRouter());
+RouterFacade.use(userRouter);
+RouterFacade.use(new DatabaseRouter().getRouter());
+RouterFacade.use(new CompanyRouter().getRouter());
+
+
+///////////////////////////////////////////////////////////////////////////////
+import {user} from "../models/userModel";
+
+RouterFacade.get("/setup", function (request : express.Request, response : express.Response) : void {
+    user.addSuperAdmin({
+        email: "bug@prova.it",
+        password: "123456ciao"
+    }).then(function (data) : void {
+        response.json(data);
+    }, function (error : Object) : void {
+        response.json(error);
+    });
+});
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+
+export default RouterFacade;
