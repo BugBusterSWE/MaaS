4fab216d497d3faaab164cef88dfb00d8491d2c8
drigoni <davider1994@gmail.com>
2016-06-06 10:31:42 +0200

    Try logout

--- src/public/src/scripts/react/navbar/navbarSuperAdmin.tsx
+++ src/public/src/scripts/react/navbar/navbarSuperAdmin.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import {Link} from "react-router";
+import sessionActionCreators from "../../actions/sessionActionCreator";
 
 /**
  *
@@ -32,20 +33,25 @@ class NavbarSuperAdmin extends React.Component<INavbarSuperAdminStateProps,
         super();
     }
 
+
     /**
      * @description
      * <p>Render method of the component.
      * It renders the super admin navbar.</p>
      * @return {JSX.Element}
      */
-    render() : JSX.Element {
+    public render() : JSX.Element {
         /* tslint:disable: max-line-length */
         return(
             <nav>
                 <div className="nav-wrapper grey darken-3">
                     <ul id="nav-mobile" className="right">
                         <li><Link id="navMail" to="/UserData">{this.props.userEmail}</Link></li>
-                        <li>Logout</li>
+                        <li>
+                            <a className="waves-effect waves-light btn" onClick={this._submitLogout.bind(this)}>
+                                Logout
+                            </a>
+                        </li>
                     </ul>
                     <ul id="nav-mobile" className="left">
                         <li><Link to="/SuperAdmin/ShowCompanies">Show companies</Link></li>
@@ -55,6 +61,14 @@ class NavbarSuperAdmin extends React.Component<INavbarSuperAdminStateProps,
             </nav>
         );
         /* tslint:enable: max-line-length */
+    }
+
+    /**
+     * @description This method execute the logout.
+     * @private
+     */
+    private _submitLogout() : void {
+        sessionActionCreators.logout();
     }
 }
 
