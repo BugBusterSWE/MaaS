cc346000991a118afa9357c8a591cd028f447a10
drigoni <davider1994@gmail.com>
2016-06-06 11:22:21 +0200

    Change login

--- src/public/src/scripts/react/components/login.tsx
+++ src/public/src/scripts/react/components/login.tsx
@@ -1,15 +1,15 @@
 import * as React from "react";
-import {Link} from "react-router";
+import {Link, browserHistory} from "react-router";
 import * as ReactDOM from "react-dom";
 import Navbar from "../navbar/navbar";
-import {PermissionLevel} from "../../stores/sessionStore"
+import sessionStore, {PermissionLevel} from "../../stores/sessionStore";
 import sessionActionCreators from "../../actions/sessionActionCreator";
-import SessionStore from "../../stores/sessionStore";
 import ErrorMessage from "./errorMessageComponent";
 
 export interface ILoginState {
-    token : string;
-    error : string;
+    isLoggedIn : boolean;
+    level : string;
+    message : string;
 }
 
 class Login extends React.Component<void, ILoginState> {
@@ -17,8 +17,9 @@ class Login extends React.Component<void, ILoginState> {
     constructor() {
         super();
         this.state = {
-            token: SessionStore.getAccessToken(),
-            error: SessionStore.getAllErrors()
+            isLoggedIn: sessionStore.isLoggedIn(),
+            level: sessionStore.getLevel(),
+            message: sessionStore.getAllErrors()
         };
 
         this._onChange = this._onChange.bind(this);
@@ -41,75 +42,78 @@ class Login extends React.Component<void, ILoginState> {
      */
 
     componentDidMount() : void {
-        SessionStore.addChangeListener(this._onChange);
+        sessionStore.addChangeListener(this._onChange);
     }
 
     componentWillUnmount() : void {
-        SessionStore.removeChangeListener(this._onChange);
+        sessionStore.removeChangeListener(this._onChange);
     }
 
     _onChange() : void {
         console.log("On Change");
-        this.setState({
-            token: SessionStore.getAccessToken(),
-            error: SessionStore.getAllErrors()
-        });
+        this.state = {
+            isLoggedIn: sessionStore.isLoggedIn(),
+            level: sessionStore.getLevel(),
+            message: sessionStore.getAllErrors()
+        };
+        if (this.state.isLoggedIn) {
+            switch (this.state.level) {
+                case PermissionLevel.SUPERADMIN:
+                    browserHistory.push("/#/SuperAdmin/ShowCompanies");
+                    break;
+                default:
+                    browserHistory.push("/#/Dashboard");
+                    break;
+            }
+        }
+
     }
 
     // TODO: passare parametro al messaggio di errore
     render() : JSX.Element {
-
-        if (!this.state.token) {
-            /* tslint:disable: max-line-length */
-            return(
-                <div>
-                    <Navbar userPermission={PermissionLevel.GUEST} />
-                    <div id="contentBody" className="container">
-                        <div id="titles">
-                            <h3>Login</h3>
-                        </div>
-                        <div className="divider"></div>
-                        <div className="row">
-                            <ErrorMessage error={this.state.error} />
-                            <form className="col s12">
-                                <div className="row">
-                                    <div className="input-field col s12">
-                                        <i className="material-icons prefix">
-                                            email
-                                        </i>
-                                        <input id="email" type="email" className="validate" ref="email"/>
-                                        <label for="email">Email</label>
-                                    </div>
-                                    <div className="input-field col s12">
-                                        <i className="material-icons prefix">
-                                            lock
-                                        </i>
-                                        <input id="password" type="password" className="validate" ref="password"/>
-                                        <label for="password">Password</label>
-                                    </div>
+        /* tslint:disable: max-line-length */
+        return(
+            <div>
+                <Navbar userPermission={PermissionLevel.GUEST} />
+                <div id="contentBody" className="container">
+                    <div id="titles">
+                        <h3>Login</h3>
+                    </div>
+                    <div className="divider"></div>
+                    <div className="row">
+                        <ErrorMessage error={this.state.message} />
+                        <form className="col s12">
+                            <div className="row">
+                                <div className="input-field col s12">
+                                    <i className="material-icons prefix">
+                                        email
+                                    </i>
+                                    <input id="email" type="email" className="validate" ref="email"/>
+                                    <label for="email">Email</label>
                                 </div>
-                            </form>
-                            <div className="col s6">
-                                <Link to= "/RecoveryPassword">
-                                        Forgot your password?
-                                </Link>
+                                <div className="input-field col s12">
+                                    <i className="material-icons prefix">
+                                        lock
+                                    </i>
+                                    <input id="password" type="password" className="validate" ref="password"/>
+                                    <label for="password">Password</label>
+                                </div>
                             </div>
-                            <div className="right">
-                                <a className="waves-effect waves-light btn" onClick={this._submitLogin.bind(this)}>
-                                        Sign in
-                                </a>
-                            </div>
+                        </form>
+                        <div className="col s6">
+                            <Link to= "/RecoveryPassword">
+                                    Forgot your password?
+                            </Link>
+                        </div>
+                        <div className="right">
+                            <a className="waves-effect waves-light btn" onClick={this._submitLogin.bind(this)}>
+                                    Sign in
+                            </a>
                         </div>
                     </div>
                 </div>
-            );
-        } else {
-            return(
-                <div>
-                    {window.location.replace("/#/SuperAdmin/ShowCompanies")}
-                </div>
-            );
-        }
+            </div>
+        );
     /* tslint:enable: max-line-length */
     }
 }
