bd14d636c37ae5d2b63550ee39ed7aab6a7ca488
drigoni <davider1994@gmail.com>
2016-06-06 16:02:27 +0200

    Add 404 error page and logout page

--- src/public/src/scripts/main.tsx
+++ src/public/src/scripts/main.tsx
@@ -5,6 +5,7 @@ import {Router, Route} from "react-router";
 import homePage from "./react/components/home";
 import LoginPage from "./react/components/login";
 import RecoveryPasswordPage from "./react/components/recoveryPassword";
+import LogoutPage from "./react/components/logout";
 // Import Dashboard
 // Import Collection
 // Import Editor
@@ -21,31 +22,20 @@ import { hashHistory } from "react-router";
 
 ReactDOM.render(
     <Router history={hashHistory}>
-        <Route path="/" component={homePage}>
-        </Route>
-        <Route path="/Home" component={homePage}>
-        </Route>
-        <Route path="/Login" component={LoginPage}>
-        </Route>
-        <Route path="/RecoveryPassword" component={RecoveryPasswordPage}>
-        </Route>
-        <Route path="/Dashboard" component={homePage}>
-        </Route>
-        <Route path="/Collection" component={homePage}>
-        </Route>
-        <Route path="/Editor" component={homePage}>
-        </Route>
-        <Route path="/Members:company_id" component={ShowCompanyMembersPage}>
-        </Route>
-        <Route path="/SuperAdmin/ShowCompanies" component={ShowCompaniesPage}>
-        </Route>
+        <Route path="/" component={homePage} />
+        <Route path="/Home" component={homePage} />
+        <Route path="/Login" component={LoginPage} />
+        <Route path="/RecoveryPassword" component={RecoveryPasswordPage} />
+        <Route path="/Logout" component={LogoutPage} />
+        <Route path="/Dashboard" component={homePage} />
+        <Route path="/Collection" component={homePage} />
+        <Route path="/Editor" component={homePage} />
+        <Route path="/Members:company_id" component={ShowCompanyMembersPage} />
+        <Route path="/SuperAdmin/ShowCompanies" component={ShowCompaniesPage} />
         <Route path="/SuperAdmin/company/:company_id"
-               component={ShowCompanyMembersPage}>
-        </Route>
-        <Route path="/SuperAdmin/AddCompany" component={AddCompanyPage}>
-        </Route>
+               component={ShowCompanyMembersPage} />
+        <Route path="/SuperAdmin/AddCompany" component={AddCompanyPage} />
         <Route path="/SuperAdmin/InviteSuperAdmin"
-               component={InviteSuperAdminPage}>
-        </Route>
+               component={InviteSuperAdminPage} />
     </Router>, document.getElementById("content")
 );
---
+++ src/public/src/scripts/react/components/error404.tsx
@@ -0,0 +1,66 @@
+import * as React from "react";
+import Navbar from "../navbar/navbar";
+import {hashHistory} from "react-router";
+import {PermissionLevel} from "../../stores/sessionStore";
+import sessionActionCreator from "../../actions/sessionActionCreator";
+
+
+/**
+ * This class represents the 404 error page.
+ *
+ * @history
+ * | Author        | Action Performed | Data       |
+ * |---------------|------------------|------------|
+ * | Davide Rigoni | Create class     | 20/05/2016 |
+ *
+ * @author Davide Rigoni
+ * @license MIT
+ */
+class Error404 extends React.Component<void , void> {
+
+    /**
+     * @description Default constructor.
+     */
+    constructor() {
+        super();
+    }
+
+    /**
+     * @description This method do the render of this class error404.
+     * @returns {JSX.Element}
+     */
+    public render() : JSX.Element {
+        /* tslint:disable: max-line-length */
+        return(
+            <div>
+                <Navbar userPermission={PermissionLevel.GUEST} />
+                <div id="contentBody" className="container">
+                    <div id="titles">
+                        <h3>Error 404</h3>
+                    </div>
+                    <div className="divider"></div>
+                    <div>
+                        <ErrorMessage error="Page not found. You will be redirected to the home page of MaaS." />
+                    </div>
+                </div>
+            </div>
+        );
+    /* tslint:enable: max-line-length */
+    }
+
+    /**
+     * @description This method will call automatically at the mount of the
+     * react component.
+     */
+    private componentDidMount() : void {
+        sessionActionCreator.logout();
+        setTimeout(() : void  => {
+            hashHistory.push("/Home")
+        }, 5000);
+    }
+}
+
+/**
+ * @description The error404 type to export.
+ */
+export default Error404;
