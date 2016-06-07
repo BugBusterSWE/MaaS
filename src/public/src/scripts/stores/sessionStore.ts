85de71f392d4554491e81369de9f14efdaaa3a93
EmanueleC <emanuele.carraro.94@gmail.com>
2016-06-07 12:00:16 +0200

    add console log

--- src/public/src/scripts/stores/sessionStore.ts
+++ src/public/src/scripts/stores/sessionStore.ts
@@ -192,6 +192,7 @@ class SessionStore extends EventEmitter {
                         message : undefined
                     }
                 } else {
+                    console.log("error " + action.actionError.message);
                     store._actionError = action.actionError;
                     store._loginResponse = {
                         token : undefined,
