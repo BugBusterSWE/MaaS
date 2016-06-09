6d77c58949e80379f855916da6adff16c5f3fd2a
drigoni <davider1994@gmail.com>
2016-06-09 15:08:02 +0200

    Add html to CompanyRegistration

--- src/public/src/scripts/react/components/CompanyRegistration.tsx
+++ src/public/src/scripts/react/components/CompanyRegistration.tsx
@@ -1,40 +1,61 @@
 import * as React from "react";
 import {Link} from "react-router";
-import Navbar from "../navbar/navbar";
-import {PermissionLevel} from "../../stores/sessionStore"
-import ErrorMessage from "./errorMessageComponent";
+import Navbar from "../../navbar/navbar";
+import {PermissionLevel} from "../../../stores/sessionStore"
+import companyActionCreator, {IAddCompanyUser, IAddCompanyName}
+    from "../../../actions/companyActionCreator";
+import * as ReactDOM from "react-dom";
+import ErrorMessage from "../errorMessageComponent";
+import companyStore from "../../../stores/companyStore";
+import sessionStore from "../../../stores/sessionStore"
 
-/**
- * This class represents the logout page.
- *
- * @history
- * | Author        | Action Performed | Data       |
- * |---------------|------------------|------------|
- * | Davide Rigoni | Create class     | 20/05/2016 |
- *
- * @author Davide Rigoni
- * @license MIT
- */
-class CompanyRegistration extends React.Component<void, void> {
+interface ICompanyRegistration {
+    message : string;
+    token : string;
+}
+
+class CompanyRegistration extends React.Component<void, ICompanyRegistration> {
 
     /**
-     * @description Default constructor
+     * @description
+     * <p>This constructor calls his super constructor.
+     * It creates an AddCompany, defines its state and
+     * binds _onChange function to "this"</p>
+     * @return {AddCompany}
      */
     constructor() {
         super();
         this.state = {
-            token: SessionStore.getAccessToken(),
-            error: SessionStore.getAllErrors()
+            message: companyStore.getAddCompanyError(),
+            token: sessionStore.getAccessToken()
         };
 
         this._onChange = this._onChange.bind(this);
     }
 
+    addCompany() : void {
+        let email : string =
+            ReactDOM.findDOMNode<HTMLInputElement>(this.refs["email"]).value;
+        let password : string =
+            ReactDOM.findDOMNode<HTMLInputElement>(this.refs["password"]).value;
+        let companyName : string =
+            ReactDOM.
+            findDOMNode<HTMLInputElement>(this.refs["companyName"]).value;
+        console.log("AddCompany React");
+        console.log(email);
+        companyActionCreator.addCompany(
+            {
+                email : email,
+                password : password
+            },
+            {
+                name : companyName
+            },
+            this.state.token
+        );
+    }
+
     // TODO: passare parametro al messaggio di errore
-    /**
-     * @description This method do the render of this class CompanyRegistration.
-     * @returns {JSX.Element}
-     */
     render() : JSX.Element {
         /* tslint:disable: max-line-length */
         return(
@@ -42,30 +63,36 @@ class CompanyRegistration extends React.Component<void, void> {
                 <Navbar userPermission={PermissionLevel.GUEST} />
                 <div id="contentBody" className="container">
                     <div id="titles">
-                        <h3>Company registration</h3>
+                        <h3>Add company</h3>
                     </div>
                     <div className="divider"></div>
                     <div className="row">
-                        <ErrorMessage error="prova" />
+                        <ErrorMessage error={this.state.message} />
                         <form className="col s12">
                             <div className="row">
                                 <div className="input-field col s12">
-                                    <i className="material-icons prefix">email</i>
-                                    <input id="email" type="text" className="validate" ref="email"/>
-                                    <label for="email">Email</label>
+                                    <input id="companyName" type="text" className="validate" ref="companyName"/>
+                                    <label for="companyName">Name of the company</label>
                                 </div>
                             </div>
                             <div className="row">
                                 <div className="input-field col s12">
-                                    <i className="material-icons prefix">password</i>
-                                    <input id="password" type="password" className="validate"  ref="password"/>
-                                    <label for="password">Password</label>
+                                    <i className="material-icons prefix">email</i>
+                                    <input id="email" type="email" className="validate"  ref="email"/>
+                                    <label for="email">Email of the super Owner</label>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="input-field col s12">
+                                    <i className="material-icons prefix">lock</i>
+                                    <input id="password" type="text" className="validate"  ref="password"/>
+                                    <label for="password">Password of the Owner</label>
                                 </div>
                             </div>
                             <div className="right">
-                                <a className="waves-effect waves-light btn" onClick={this.UpdateProfile.bind(this)}>
+                                <a className="waves-effect waves-light btn" onClick={this.addCompany.bind(this)}>
                                     <i className="material-icons left">done</i>
-                                    Registration
+                                    Add Company
                                 </a>
                             </div>
                         </form>
@@ -75,6 +102,31 @@ class CompanyRegistration extends React.Component<void, void> {
         );
         /* tslint:enable: max-line-length */
     }
+
+    /*
+     following methods are automatically called.
+     */
+
+    private componentDidMount() : void {
+        companyStore.addChangeListener(this._onChange);
+    }
+
+    private componentWillUnmount() : void {
+        companyStore.removeChangeListener(this._onChange);
+    }
+
+    private _onChange() : void {
+        console.log("onChange addCompany");
+        let errorMessage : string = "";
+        if (companyStore.isErrored()) {
+            errorMessage = companyStore.getAddCompanyError()
+        }
+        this.setState({
+            message : errorMessage,
+            token : sessionStore.getAccessToken()
+        });
+    }
 }
+
 
 export default CompanyRegistration;
