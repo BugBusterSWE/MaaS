5a6cd8b7d7675d1617a4de0364ca337f6c0e1bec
Andrea Mantovani <andreamnt94@gmail.com>
2016-06-13 12:23:23 +0200

    Correct lint error
    See issue #49

--- src/public/src/scripts/utils/sessionAPI.ts
+++ src/public/src/scripts/utils/sessionAPI.ts
@@ -24,7 +24,7 @@ class SessionAPIs {
      * @returns {Promise<T>|Promise} The result or the error
      */
     public login(email : string, password : string) : Promise<Object> {
-	let encryptedPassword = crypto.SHA256(crypto.SHA256(password));
+        let encryptedPassword : string = crypto.SHA256(crypto.SHA256(password));
 
         return new Promise(
             function(
