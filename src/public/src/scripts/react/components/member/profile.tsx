56bfeeb19171fd629369b7994b595a1acba9ee9c
drigoni <davider1994@gmail.com>
2016-07-01 11:55:18 +0200

    fix error on removeCompany

--- src/public/src/scripts/react/components/member/profile.tsx
+++ src/public/src/scripts/react/components/member/profile.tsx
@@ -251,6 +251,8 @@ class Profile extends React.Component<void, IProfileState> {
     private _removeCompany() : void {
         let companyValue : string =
             ReactDOM.findDOMNode<HTMLInputElement>(this.refs["company"]).value;
+        console.log(sessionStore.getAccessToken());
+        console.log(sessionStore.getUserCompanyID());
         companyActionCreator.removeCompany({
             token : sessionStore.getAccessToken(),
             company_id : sessionStore.getUserCompanyID(),
