f5c0e93354552d62f5b84e490506af49f5355948
EmanueleC <emanuele.carraro.94@gmail.com>
2016-07-04 23:07:58 +0200

    add console log for debug

--- src/public/src/scripts/stores/dslStore/dashboardStore.ts
+++ src/public/src/scripts/stores/dslStore/dashboardStore.ts
@@ -56,6 +56,7 @@ class DashboardStore extends EventEmitter {
      */
     public updateData(data : IDashboard) : void {
         console.log("update dashboard");
+        console.log(data);
         this.dashboard = data;
     }
 
