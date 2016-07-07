70acb1aeebc4f4c8431a17e2114a5aa689ae1611
EmanueleC <emanuele.carraro.94@gmail.com>
2016-07-07 20:47:31 +0200

    add collectionStore, update view

--- src/public/src/scripts/react/components/dslTables/collection.tsx
+++ src/public/src/scripts/react/components/dslTables/collection.tsx
@@ -7,119 +7,14 @@ import * as React from "react";
 import {browserHistory} from "react-router";
 import sessionStore, {PermissionLevel} from "../../../stores/sessionStore";
 import Navbar from "../navbar/navbar";
+import store from "../../../stores/dslStore/collectionStore";
+import collectionActionCreator from
+    "../../../actions/dslActionCreator/collectionActionCreator";
 
 export interface ICollectionState {
     index : IIndexPage;
 }
 
-let headerCollection : IHeaderIndexPage[] = [
-    {
-        label : "doc_id",
-        name : "Document id",
-        selectable : false,
-        sortable : false
-    },
-    {
-        label : "doc_name",
-        name : "Document name",
-        selectable : true,
-        sortable : true
-    },
-    {
-        label : "city",
-        name : "city",
-        selectable : false,
-        sortable : true
-    },
-    {
-        label : "email",
-        name : "email",
-        selectable : false,
-        sortable : false
-    },
-    {
-        label : "age",
-        name : "age",
-        selectable : false,
-        sortable : true
-    },
-    {
-        label : "fullname",
-        name : "full name",
-        selectable : false,
-        sortable : false
-    }
-];
-
-let interactiveDocuments : IInteractiveDocument[] = [
-    {
-        id : "D01",
-        label : "document 01",
-        name : "document 01",
-        data : {
-            city : "Padova",
-            email : "emanuele94@gmail.com",
-            age : "21",
-            fullname : "Emanuele Carraro"
-        },
-        selectable : true,
-        sortable : false
-    },
-    {
-        id : "D02",
-        label : "document 02",
-        name : "document 02",
-        data : {
-            city : "Padova",
-            email : "polpetta94@gmail.com",
-            age : "21",
-            fullname : "Davide Polonio"
-        },
-        selectable : true,
-        sortable : true
-    },
-    {
-        id : "D03",
-        label : "document 03",
-        name : "document 03",
-        data : {
-            city : "Padova",
-            email : "korut94@gmail.com",
-            age : "21",
-            fullname : "Andrea Mantovani"
-        },
-        selectable : false,
-        sortable : false
-    },
-    {
-        id : "D04",
-        label : "document 04",
-        name : "document 04",
-        data : {
-            city : "Padova",
-            email : "drigoni94@gmail.com",
-            age : "21",
-            fullname : "Davide Rigoni"
-        },
-        selectable : false,
-        sortable : false
-    }
-];
-
-let allDocuments : IIndexDoc[] = [{
-    id : "ALLDOCS",
-    data : interactiveDocuments
-}];
-
-let indexPage : IIndexPage = {
-    id : "INDEX1",
-    name : "document name",
-    label : "document label",
-    numdocs : 3,
-    perpage : 5,
-    header : headerCollection,
-    documents : allDocuments
-};
 
 /* This is the Index Page */
 
@@ -132,7 +27,7 @@ class Collection extends React.Component<void, ICollectionState> {
     constructor() {
         super();
         this.state = {
-            index : indexPage
+            index : store.getIndex()
         };
         this._onChange = this._onChange.bind(this);
         this._orderField = this._orderField.bind(this);
@@ -235,6 +130,8 @@ class Collection extends React.Component<void, ICollectionState> {
         if (!(sessionStore.checkPermission(PermissionLevel.MEMBER))) {
             browserHistory.push("/Error403")
         }
+        store.addChangeListener(this._onChange);
+        collectionActionCreator.getIndexPage();
     }
 
     /**
@@ -242,6 +139,7 @@ class Collection extends React.Component<void, ICollectionState> {
      */
     private componentWillUnmount() : void {
         console.log("collection component did UNmount");
+        store.removeChangeListener(this._onChange);
     }
 
     /**
@@ -249,6 +147,9 @@ class Collection extends React.Component<void, ICollectionState> {
      */
     private _onChange() : void {
         console.log("onChange collection");
+        this.setState({
+            index : store.getIndex()
+        });
     }
 
     /**
