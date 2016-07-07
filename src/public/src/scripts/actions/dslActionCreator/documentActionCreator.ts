e6e6af11608bb8bcbc7e73069c39129b84f20bc5
EmanueleC <emanuele.carraro.94@gmail.com>
2016-07-07 19:33:52 +0200

    Add documentActionCreator

--- src/public/src/scripts/actions/dslActionCreator/documentActionCreator.ts
+++ src/public/src/scripts/actions/dslActionCreator/documentActionCreator.ts
@@ -1,3 +1,41 @@
+import Dispatcher, {Action, ActionError} from "../../dispatcher/dispatcher";
+import documentAPIs from "../../utils/dslAPI/documentAPI";
+import {IDocument} from "../../utils/dslDefinitions";
+
+export let DispatcherCellData : Dispatcher<Action<IDocument>> =
+    new Dispatcher<Action<IDocument>>();
+
 /**
- * Created by emanuele on 04/07/16.
+ * This class represents the creator of the action of the document.
+ *
+ * @history
+ * | Author           | Action Performed                        | Data       |
+ * |------------------|-----------------------------------------|------------|
+ * | Emanuele Carraro | Create DocumentActionCreator class      | 05/07/2016 |
+ *
+ * @author Emanuele Carraro
+ * @license MIT
+ *
  */
+class DocumentActionCreator {
+
+    /**
+     * @description Dispatch the action to get the data of the document.
+     */
+    public getDocumentData() : void {
+        console.log("get document data");
+        documentAPIs
+            .getCellData()
+            .then(function (data : IDocument) : void {
+                DispatcherCellData.dispatch({
+                    actionData : data,
+                    actionError : undefined
+                })
+            })
+    }
+
+}
+
+let documentActionCreator : DocumentActionCreator =
+    new DocumentActionCreator();
+export default documentActionCreator;
